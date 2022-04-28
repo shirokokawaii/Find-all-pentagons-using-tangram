85c33661248181c5ac4463362df976c3e7ea3703
as312d <as312d@gmail.com>
2022-04-28 11:41:50 +0800

    erase same shape in each search

--- Tangram/src/LIAO/Shape.java
+++ Tangram/src/LIAO/Shape.java
@@ -6,7 +6,7 @@ public class Shape {
 	ArrayList<Point> point;
 	Queue<Shape> shapesSet = new LinkedList<Shape>();
 	Queue<String> orderSet = new LinkedList<String>();
-	
+
 	public boolean contains(Shape shape) {
 		if(shapesSet.contains(shape)) {
 			return true;
@@ -16,8 +16,5 @@ public class Shape {
 	public void add(Shape shape, String edge) {
 		shapesSet.add(shape);
 		orderSet.add(edge);
-	}
-	public void getLoopValue(){
-		
 	}
 }
