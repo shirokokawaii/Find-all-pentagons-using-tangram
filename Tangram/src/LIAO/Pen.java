33d59be8e10f00e2c1678d5dc332614233dce34f
锟斤拷 <68645346+shirokokawaii@users.noreply.github.com>
2022-04-29 19:08:06 +0800

    Complete Pen.java

--- Tangram/src/LIAO/Pen.java
+++ Tangram/src/LIAO/Pen.java
@@ -3,50 +3,108 @@ package LIAO;
 import java.awt.*;
 import java.util.*;
 import javax.swing.*;
-import LIAO.Tangram;
 
-public class Pen{
+public class Pen{//usage:First create an object of pen, then call the method "draw".
     int x1, x2, y1, y2 = 0;
     Graphics graphics;
-    Shape shape = new Shape();
     JPanel jpanel = new JPanel();
-    public Pen(Shape shape, JPanel jpanel){
-        this.shape = shape;
-        this.jpanel = jpanel;
-    }
 
-    public void calculatePoint(Shape shape, ArrayList<Double> x, ArrayList<Double> y){
-        double originalX = 0;
+    public void draw(Shape shape, JPanel jpanel){
+        this.jpanel = jpanel;
+        this.graphics = jpanel.getGraphics();
+        ArrayList<Double> x = new ArrayList<>();
+        ArrayList<Double> y = new ArrayList<>();
+        double originalX = 0;//start painting from x=0, y=0
         double originalY = 0;
+        double nextX = 0;
+        double nextY = 0;
+        double nextAngle = 0;
         int originalPoint = 0;
-        double orginalAngle = -180;
+        double originalAngle = 0;
+        x.add(originalX);
+        y.add(originalY);
+        Shape shapeFirst = shape.shapesSet.poll();
+        Shape shapeSecond;
+        double nextPoint = 0;
+        boolean nextDirection;
         while(!shape.shapesSet.isEmpty()){
-            Shape shapeTem = shape.shapesSet.poll();
-            int len = shapeTem.size();
+            shapeSecond = shape.shapesSet.poll();
+            nextPoint = shape.pointOrder1.poll();
+            nextDirection = shape.orderDirection.poll();
+            int len = shapeFirst.size();
+
             for(int i=0;i<len;i++){
-                int index = originalPoint;
-                double length = shapeTem.getLength(i);
-                double angle = 45*shapeTem.getAngel(i);
-                double diffX = length*Math.cos(angle);
-                double diffY = length*Math.sin(angle);
+                originalAngle += 180;
+                if(originalAngle >=360){
+                    originalAngle -= 360;
+                }
+                double length = shapeFirst.getLength(originalPoint);
+                double diffAngle = 45*shapeFirst.getAngel(originalPoint);
+                originalAngle += diffAngle;
+                if(i==nextPoint){
+                    nextX = originalX;
+                    nextY = originalY;
+                    if(nextDirection==true){
+                        nextAngle = originalAngle;//clockwise
+                    }
+                    else{
+                        nextAngle = originalAngle-diffAngle; //anti-clockwise
+                    }
+                }
+                double diffX = length*Math.cos(originalAngle);
+                double diffY = length*Math.sin(originalAngle);
                 originalX += diffX;
                 originalY += diffY;
                 x.add(originalX);
                 y.add(originalY);
+                originalPoint++;
+                if(originalPoint == len){
+                    originalPoint = 0;
+                }
+            }
+            drawLine(x, y);
+            x.clear();
+            y.clear();
+            shapeFirst = shapeSecond;
+            originalX = nextX;
+            originalY = nextY;
+            originalAngle = nextAngle;
+        }
+
+        int len = shapeFirst.size();
+        for(int i=0;i<len;i++){//The last shape
+            originalAngle += 180;
+            if(originalAngle >=360){
+                originalAngle -= 360;
+            }
+            double length = shapeFirst.getLength(originalPoint);
+            double angle = originalAngle + 45*shapeFirst.getAngel(originalPoint);
+            originalAngle = angle;
+            double diffX = length*Math.cos(angle);
+            double diffY = length*Math.sin(angle);
+            originalX += diffX;
+            originalY += diffY;
+            x.add(originalX);
+            y.add(originalY);
+            originalPoint++;
+            if(originalPoint == len){
+                originalPoint = 0;
             }
         }
+        drawLine(x, y);
     }
 
-    public void drawLine(ArrayList<Integer> x, ArrayList<Integer> y){
+
+    public void drawLine(ArrayList<Double> x, ArrayList<Double> y){
         int len = x.size();
-        for(int i=0;i<=len;i++){
-            int x1 = x.get(i);
-            int y1 = y.get(i);
+        for(int i=0;i<len;i++){
+            int x1 = (int)Math.round(x.get(i));
+            int y1 = (int) Math.round(y.get(i));
             if(i+1 == len){
-                i = 0;
+                i = -1;
             }
-            int x2 = x.get(i+1);
-            int y2 = y.get(i+1);
+            int x2 = (int)Math.round(x.get(i+1));
+            int y2 = (int) Math.round(y.get(i+1));
             graphics.drawLine(x1, y1, x2, y2);
         }
     }
