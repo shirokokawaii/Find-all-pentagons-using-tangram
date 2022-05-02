ffbd6c2b77b9d4af4fffe7312e3454daba884859
锟斤拷 <68645346+shirokokawaii@users.noreply.github.com>
2022-05-03 02:24:48 +0800

    Update Algorithm.java

--- Tangram/src/LIAO/Algorithm.java
+++ Tangram/src/LIAO/Algorithm.java
@@ -39,7 +39,7 @@ public class Algorithm {
 	}
 
 	public LinkedList<Shape> bfsSearch(Shape shapeIn) {
-		int acuracy = 3;//0:highest
+		int acuracy = 2;//0:highest
 		int index = 1;
 		LinkedList<Shape> set1 = new LinkedList<Shape>();
 		set1.offer(shapeIn);
