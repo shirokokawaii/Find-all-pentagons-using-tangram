a2d91cdc3dbd29504096596fa0d89847dd01ec00
锟斤拷 <68645346+shirokokawaii@users.noreply.github.com>
2022-05-08 21:04:57 +0800

    Update Test.java

--- Tangram/src/LIAO/Test.java
+++ Tangram/src/LIAO/Test.java
@@ -1,30 +1,29 @@
-package LIAO;
+// package LIAO;
 
-import static LIAO.entity.Tangram.*;
+// import static LIAO.entity.Tangram.*;
 
-import java.util.LinkedList;
-import javax.swing.*;
+// import java.util.LinkedList;
+// import javax.swing.*;
 
-public class Test {
-	public static void main(String[] args) {
-    	
-         JFrame jf = new JFrame("visiable");
-         JPanel jp = new JPanel();
-         jf.add(jp);
-         jp.setSize(600, 600);
-         jf.setSize(600, 600);
-         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         jf.setVisible(true);
-         int X = 250;
-         int Y = 300;
-         int size = 50;
+// public class Test {
+// 	public static void main(String[] args) {//A test class for developer
+//          JFrame jf = new JFrame("visiable");
+//          JPanel jp = new JPanel();
+//          jf.add(jp);
+//          jp.setSize(600, 600);
+//          jf.setSize(600, 600);
+//          jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//          jf.setVisible(true);
+//          int X = 250;
+//          int Y = 300;
+//          int size = 50;
          
-         Algorithm algorithm = new Algorithm(S0, S1, S2, S3, S4, S5, S6, S7);
-         algorithm.displayWhileCalculating(jf, jp, X, Y, size);
-         LinkedList<LinkedList<Shape>> answerList = new LinkedList<>();
-         algorithm.bfsSearch();
-         //algorithm.dfsSearch();
-         //algorithm.aStarSearch();
-         answerList = algorithm.getAnswerList();
-    }
-}
+//          Algorithm algorithm = new Algorithm(S0, S1, S2, S3, S4, S5, S6, S7);
+//          algorithm.displayWhileCalculating(jf, jp, X, Y, size);
+//          LinkedList<LinkedList<Shape>> answerList = new LinkedList<>();
+//          algorithm.bfsSearch();
+//          //algorithm.dfsSearch();
+//          //algorithm.aStarSearch();
+//          answerList = algorithm.getAnswerList();
+//     }
+// }
